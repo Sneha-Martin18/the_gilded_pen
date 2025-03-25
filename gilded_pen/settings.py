467fa107155332e76d9148ebccf77af9662cdecb
Sneha-Martin18 <96016242+Sneha-Martin18@users.noreply.github.com>
2025-03-25 13:05:47 +0530

    Update settings.py

--- gilded_pen/settings.py
+++ gilded_pen/settings.py
@@ -24,16 +24,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tf)pq*m455n=v4+#=3jeu858r-&p#!ozufhc^q3uxo8c14#ftr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
- ALLOWED_HOSTS = [
+ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
- ]
-  CSRF_TRUSTED_ORIGINS = [
+    'blogmdn-c7gyfqbjarggb4ea.centralus-01.azurewebsites.net',
+]
+CSRF_TRUSTED_ORIGINS = [
     "https://blogmdn-c7gyfqbjarggb4ea.centralus-01.azurewebsites.net",
 ]
-
 
 
 # Application definition
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
